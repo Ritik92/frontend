8e30c2681a4d19d64e7dd7498ff4b0458da6ecfa
Ritik92 <kartik292004@gmail.com>
2024-07-14 17:00:51 +0530

    hi

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,33 @@
+// app/api/auth/[...nextauth]/route.ts
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import GoogleProvider from 'next-auth/providers/google'
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+        name: 'Credentials',
+        credentials: {
+          username: { label: "Username", type: "text", placeholder: "Enter Username" },
+          password: { label: "Password", type: "password",placeholder: "Enter Password" }
+        },
+        async authorize(credentials, req) {
+            const { username, password } = credentials as { username: string; password: string };
+
+            // Hardcoded check (for demonstration only, not for production use)
+            if (username === "john" && password === "12345") {
+              return { id: "1", name: "John Doe", email: "john@example.com" }; 
+            }
+    
+            return null;
+        }
+      })
+    ,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_PASSWORD!,
+    }),
+  ],
+})
+
+export { handler as GET, handler as POST }
