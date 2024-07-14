79ee8d76c2ee0047829c2c947f556b8e11d3c89a
Ritik92 <147306147+Ritik92@users.noreply.github.com>
2024-07-14 19:06:06 +0530

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
 
-const handler = NextAuth({
+const handler =  NextAuth({
   providers: [
     CredentialsProvider({
         name: 'Credentials',
